1017b8210d7b46ff6a66dfaf681310494711416c
Fabio Serragnoli <serragnoli@gmail.com>
2016-05-15 19:30:03 +0100

    Create JavaPongActor

---
+++ src/main/java/com/example/JavaPongActor.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Status;
+import akka.japi.pf.ReceiveBuilder;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+
+public class JavaPongActor extends AbstractActor {
+
+    @Override
+    public PartialFunction<Object, BoxedUnit> receive() {
+        return ReceiveBuilder
+                .matchEquals("Ping", s -> sender().tell("Pong", ActorRef.noSender()))
+                .matchAny(x -> sender().tell(new Status.Failure(new Exception("Unknown message")), self()))
+                .build();
+    }
+}
